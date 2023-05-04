df6b8f0c84d963f69e6bad46f15a1a2618f83ffe
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-05-04 23:06:55 +0900

    불필요 코드 삭제

--- class/pages/_app.js
+++ class/pages/_app.js
@@ -1,14 +1,3 @@
-import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
-
 export default function App({ Component, pageProps }) {
-  const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(), // 나중에 할것
-  });
-
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+  return <Component {...pageProps} />;
 }
