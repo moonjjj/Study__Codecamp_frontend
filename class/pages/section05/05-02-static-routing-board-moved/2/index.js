2546d8239405ec6ca40d1fb6e4d7172d59ca4bb6
Happy Hee <56466589+8happyhee@users.noreply.github.com>
2023-05-05 18:24:45 +0900

    [학습] 05-02-static-routing-board

---
+++ class/pages/section05/05-02-static-routing-board-moved/2/index.js
@@ -0,0 +1,7 @@
+/**
+ * 05-02-static-routing-board 수업
+ */
+
+export default function StaticRoutingMovedPage() {
+  return <div>2번 게시글 이동이 완료되었습니다.</div>;
+}
---
+++ class/pages/section05/05-02-static-routing-board-moved/index.js
@@ -0,0 +1,7 @@
+/**
+ * 05-02-static-routing-board 수업
+ */
+
+export default function StaticRoutingMovedPage() {
+  return <div>페이지 이동이 완료되었습니다.</div>;
+}
